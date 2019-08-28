0b3516f518b9577fade4f7f82687b664caf7a967
Rafael Nunes dos Santos <studiosafeway@gmail.com>
2019-08-28 12:04:30 -0300

    Criacao das Entidades

---
+++ QuyckBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,32 @@
+﻿using QuickyBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickyBuy.Dominio.Entidades
+{
+   public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+
+        public string Cep { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+
+        //Pedido teve ter pelo meno um item de pedido
+        //ou muitos itens de pedidos
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+
+    }
+}
---
+++ QuyckBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace QuickyBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+        //Usuario pode ter nunhum ou muitos pedidos
+        public ICollection<Pedido> Pedidos { get; set; }
+
+
+    }
+}
