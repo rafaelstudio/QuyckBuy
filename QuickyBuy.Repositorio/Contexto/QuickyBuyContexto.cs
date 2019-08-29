549b5cca36969b49b29a777762292bdd108e4e30
Rafael Santos <rafael.santos@onlineapp.com.br>
2019-08-29 09:49:46 -0300

    Adicionado Contexto, para conexao com o banco de dados mysql

---
+++ QuickyBuy.Repositorio/Contexto/QuickyBuyContexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickyBuy.Dominio.Entidades;
+using QuickyBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickyBuy.Repositorio.Contexto
+{
+    class QuickyBuyContexto: DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItemPedidos { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+    }
+}
